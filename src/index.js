d1b33b22a944794baf9c2350c5f25606fec026d3
vilius.ramulionis <vilius.ramulionis@gmail.com>
2025-04-06 11:34:54 +0300

    selenium connection

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { addonBuilder, serveHTTP, publishToCentral }  = require('stremio-addon-sdk')
+const { Builder, Browser }  = require('selenium-webdriver')
 
 const builder = new addonBuilder({
     id: 'org.infospread',
@@ -12,13 +13,17 @@ const builder = new addonBuilder({
     resources: ['stream'],
     types: ['movie'],
     idPrefixes: ['tt']
-})
+});
 
 // takes function(args)
-builder.defineStreamHandler(function(args) {
-    console.log(args);
+builder.defineStreamHandler(async (args) => {
+    driver = await new Builder().forBrowser(Browser.CHROME).build();
+
+    const res = await driver.get('https://www.selenium.dev/selenium/web/web-form.html');
+    console.log(await driver.getTitle());
+    
     const stream = { url: 'http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_30fps_normal.mp4' }
     return Promise.resolve({ streams: [stream] })
-})
+});
 
 serveHTTP(builder.getInterface(), { port: process.env.PORT || 7000 })
